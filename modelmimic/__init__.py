23dfb50667cea0f62fbb5f802c8acac0eda38daa
Michael Kelleher <kelleher.mike@gmail.com>
2024-05-14 13:18:43 -0500

    Update req Python version, project URLs

--- modelmimic/__init__.py
+++ modelmimic/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = (0, 0, 1)
+__version_info__ = (0, 0, 2)
 __version__ = ".".join(str(_vi) for _vi in __version_info__)
 logo = r"""
 |-------------------------------------------------|
